d38bde14aa8e51124985f63ff45164e10bf3ea64
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-11-09 16:07:35 +0000

    adding blog and codestar folder

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+from flask import flask
+from django.shortcuts import render
+from .models import Post
+from .forms import CommentForm
 
 
-
-
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codestar.settings')
+    os.environ["DATABASE_URL"] = "Paste in Heroku DATABASE_URL Link"
+    os.environ["SECRET_KEY"] = "hemlignyckel1500"
 
 
 if __name__ == '__main__':
