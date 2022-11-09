d0c32617e2d7bddf3eb2ec790a7365d4c5fea327
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-11-09 14:39:41 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
